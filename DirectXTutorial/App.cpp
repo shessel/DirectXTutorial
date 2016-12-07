3f011979db6e81fa525b0e4d8133cc8776e6339c
shessel <shessel@users.noreply.github.com>
2016-12-07 22:32:14 +0100

    add some event handlers

--- DirectXTutorial/App.cpp
+++ DirectXTutorial/App.cpp
@@ -10,23 +10,42 @@ using namespace Windows::Foundation;
 using namespace Windows::Graphics::Display;
 using namespace Platform;
 
-ref class App sealed : public IFrameworkView
-{
+ref class App sealed : public IFrameworkView {
 public:
-virtual void Initialize(CoreApplicationView^ AppView)
-	{
+virtual void Initialize(CoreApplicationView^ AppView) {
 		AppView->Activated += ref new TypedEventHandler
 			<CoreApplicationView^, IActivatedEventArgs^>(this, &App::OnActivated);
 	}
-	virtual void SetWindow(CoreWindow^ Window) {}
+
+	virtual void SetWindow(CoreWindow^ Window) {
+		Window->PointerPressed += ref new TypedEventHandler
+			<CoreWindow^, PointerEventArgs^>(this, &App::PointerPressed);
+		Window->KeyDown += ref new TypedEventHandler
+			<CoreWindow^, KeyEventArgs^>(this, &App::KeyDown);
+	}
+
 	virtual void Load(String^ EntryPoint) {}
-	virtual void Run() {}
+
+	virtual void Run() {
+		CoreWindow^ Window = CoreWindow::GetForCurrentThread();
+		Window->Dispatcher->ProcessEvents(CoreProcessEventsOption::ProcessUntilQuit);
+	}
+
 	virtual void Uninitialize() {}
 
-	void OnActivated(CoreApplicationView^ CoreAppView, IActivatedEventArgs^ Args)
-	{
+	void OnActivated(CoreApplicationView^ CoreAppView, IActivatedEventArgs^ Args) {
 		CoreWindow^ Window = CoreWindow::GetForCurrentThread();
 		Window->Activate();
+	}
+
+	void PointerPressed(CoreWindow^ Window, PointerEventArgs^ Args) {
+		MessageDialog Dialog("Hello there!", "Whoop whoop!");
+		Dialog.ShowAsync();
+	}
+
+	void KeyDown(CoreWindow^ Window, KeyEventArgs^ Args) {
+		MessageDialog Dialog(Args->VirtualKey.ToString(), "Key!");
+		Dialog.ShowAsync();
 	}
 };
 
@@ -35,15 +54,13 @@ virtual void Initialize(CoreApplicationView^ AppView)
 ref class AppSource sealed : IFrameworkViewSource
 {
 public:
-	virtual IFrameworkView^ CreateView()
-	{
+	virtual IFrameworkView^ CreateView() {
 		return ref new App();
 	}
 };
 
 [MTAThread]    // define main() as a multi-threaded-apartment function
-int main(Array<String^>^ args)
-{
+int main(Array<String^>^ args) {
 	CoreApplication::Run(ref new AppSource());
 	return 0;
 }
